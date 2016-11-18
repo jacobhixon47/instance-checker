b6089e7784912a0395fbad594879694b3ac49f6d
Jacob Hixon <jacobhixon47@gmail.com>
2016-11-17 23:54:55 -0800

    pass integration spec for displaying results of word count.

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/instance-checker')
+require('capybara/rspec')
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/instances_result') do
+  @string = params.fetch('string')
+  @word = params.fetch('word')
+  @results = @string.instance_check(@word)
+  erb(:results)
 end
--- spec/rps_integration_spec.rb
+++ spec/rps_integration_spec.rb
@@ -1,14 +1,14 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('name of the path', {:type => :feature}) do
-#   it('what it does') do
-#     visit('/')
-#     select('option', :from => 'name of select tag')
-#     fill_in('title', :with => 'green eggs and ham')
-#     click_button('Button Text')
-#     expect(page).to have_content('output')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('instances result path', {:type => :feature}) do
+  it('displays the number of instances of the inputted word within the inputted string ') do
+    visit('/')
+    fill_in('string', :with => 'dogs and cats and dogs and mice and dogs')
+    fill_in('word', :with => 'dogs')
+    click_button('Get Schwifty')
+    expect(page).to have_content('Instances: 3')
+  end
+end
